f219d44da274ba3eed093c0f37fab34453805371
robertoalvarezm <roberto.carlos.avarez@gmail.com>
2022-08-04 13:24:53 -0500

    Vignette

---
+++ R/subset_heatmap.R
@@ -0,0 +1,26 @@
+subset_heatmap <- function(x, especies = NULL, genes = NULL) {
+  # subset matrix
+  x_subset <- x[genes,especies]
+
+  # plot heatmap
+  ComplexHeatmap::Heatmap(x_subset,
+                          cluster_columns = FALSE,
+                          heatmap_legend_param = list(title = "log2"))
+}
+
+
+subset_heatmap(expresion_genes,
+               especies = c("especie_a", "especie_b","especie_c"),
+               genes = c("gene_d","gene_e","gene_f"))
+
+
+subset_heatmap_filtered <- function(x,threshold) {
+  # subset matrix
+  x_subset<-which(expresion_genes >= threshold,arr.ind = TRUE)
+  # plot heatmap
+  ComplexHeatmap::Heatmap(x_subset,
+                          cluster_columns = FALSE,
+                          heatmap_legend_param = list(title = "log2"))
+}
+
+
